b7d6f7f2f96c6a55086f1139920887fead00999c
davronweb009 <davronweb07@mail.ru>
2025-01-08 17:20:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let number = prompt('Sening isming nima?')
+let user = prompt(' Sening familyang nima?')
+let box = prompt('Sening yoshing nechida?')
+let info = prompt('Sen qayerda yashaysan?')
+let code = prompt('Senga yoqtiradigan moshina nomi?')
+alert(`Mening ismim ${number} familiyam ${user} yoshim ${box} yashash joyim ${info} yoqtiradigan moshinam ${code}`)
